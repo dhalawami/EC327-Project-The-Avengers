72d60c6ade3c70c8875653ae5ff862fbf0e247c2
dhalawami <65142405+dhalawami@users.noreply.github.com>
2020-12-04 20:28:23 +0300

    This is my game

---
+++ Game/app/src/main/java/com/example/game0/Hole.java
@@ -0,0 +1,50 @@
+package com.example.game0;
+
+import android.content.res.Resources;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Rect;
+
+import static com.example.game0.GameView.screenRatioX;
+import static com.example.game0.GameView.screenRatioY;
+
+public class Hole {
+
+    public double speed = 10;
+    int x = 0, y, width, height;
+    Bitmap hole;
+
+    Hole (Resources res) {
+        hole = BitmapFactory.decodeResource(res, R.drawable.hole);
+
+        //speed = GameView.speed;
+
+        width = hole.getWidth();
+        height = hole.getHeight();
+
+        // reducing size
+        width /= 8;
+        height /= 8;
+
+        //making it compatible
+        //width *= (int) screenRatioX;
+        //height *= (int) screenRatioY;
+
+        //resizing bitmap
+        hole = Bitmap.createScaledBitmap(hole, width, height, false);
+
+        // setting initial location of hole
+        y = -height;
+
+    }
+
+
+    Bitmap getHole() {
+        return hole;
+    }
+
+    Rect getCollisionShape () {
+        return new Rect(x, y, x + width, y + height);
+    }
+
+}
