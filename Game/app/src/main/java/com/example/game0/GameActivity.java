d91f623fc315041b73333c7917332de5dcd16028
jsad2023 <60908174+jsad2023@users.noreply.github.com>
2020-12-06 14:10:08 -0500

    Updated GameActivity to include Sensor data

--- Game/app/src/main/java/com/example/game0/GameActivity.java
+++ Game/app/src/main/java/com/example/game0/GameActivity.java
@@ -4,6 +4,10 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
 import android.graphics.Point;
 import android.media.MediaPlayer;
 import android.os.Bundle;
@@ -12,12 +16,20 @@ import android.view.WindowManager;
 import android.widget.Button;
 import android.widget.Toast;
 
-public class GameActivity extends AppCompatActivity {
+public class GameActivity extends AppCompatActivity implements SensorEventListener {
 
     MediaPlayer player;
     private GameView gameView;
     //private Button StartAgain;
     //SharedPreferences sharedPreferences;
+    private SensorManager sensorManager;
+    private Sensor mAccelerometer;
+    private Sensor mMagneticField;
+    private float[] gravityVector;
+    private float[] geomagneticVector;
+    public float roll;
+    public float pitch;
+    public float azimuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -53,7 +65,18 @@ public class GameActivity extends AppCompatActivity {
             startActivity(new Intent(GameActivity.this, MainActivity.class));
 
         }*/
+        //Sensors data
+        sensorManager = (SensorManager) this.getSystemService(SENSOR_SERVICE);
+        mMagneticField = sensorManager.getDefaultSensor(Sensor.TYPE_MAGNETIC_FIELD);
+        mAccelerometer = sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
+        gravityVector = new float[3];
+        geomagneticVector = new float[3];
 
+    }
+    @Override
+    public final void onAccuracyChanged(Sensor sensor, int accuracy) {
+
+        this.updateRoll();
     }
 
     @Override
@@ -64,12 +87,41 @@ public class GameActivity extends AppCompatActivity {
             player.pause();
         }
         gameView.pause();
+        sensorManager.unregisterListener(this);
 
+    }
+    
+    @Override
+    public final void onSensorChanged(SensorEvent event) {
+
+        // if accelerometer changes values
+        if(event.sensor.getType() == Sensor.TYPE_ACCELEROMETER) {
+            System.arraycopy(event.values, 0, gravityVector, 0, 3);
+            // if geomagnetic sensor is different
+        } else if(event.sensor.getType() == Sensor.TYPE_MAGNETIC_FIELD) {
+            System.arraycopy(event.values, 0, geomagneticVector, 0, 3);
+        }
+
+        this.updateRoll();
+    }
+     private void updateRoll() {
+
+        float[] R = new float[9];
+        boolean success = SensorManager.getRotationMatrix(R, null, gravityVector, geomagneticVector);
+        if(success) {
+            float[] orientation = new float[3];
+            SensorManager.getOrientation(R, orientation);
+            azimuth = orientation[0];
+            pitch = orientation[1];
+            roll = orientation[2];
+        }
     }
 
     @Override
     protected void onResume() {
         super.onResume();
+        sensorManager.registerListener(this, mAccelerometer, SensorManager.SENSOR_DELAY_NORMAL);
+        sensorManager.registerListener(this, mMagneticField, SensorManager.SENSOR_DELAY_NORMAL);
         gameView.resume();
         player.start();
 
